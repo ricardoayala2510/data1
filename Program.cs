f9addad32ce9f815975269cc460346b8eaffa010
ricardoayala2510 <143136224+ricardoayala2510@users.noreply.github.com>
2024-08-30 12:11:07 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿public class Algorithms
+{
+    //2.1
+    public int Pow(int baseValue, int exponent)
+    {
+        if (exponent == 0)
+            return 1;
+        return baseValue * Pow(baseValue, exponent - 1);
+    }
+
+    //2.5
+    public static long PowerOf2Iterative(int n)
+    {
+        long result = 1;
+        for (int i = 0; i < n; i++)
+        {
+            result *= 2;
+        }
+        return result;
+    }
+
+    public static long PowerOf2Recursive(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        return 2 * PowerOf2Recursive(n - 1);
+    }
+
+    public static long SuperPowerOf2(int n)
+    {
+        if (n <= 30)
+        {
+            return PowerOf2Recursive(n);
+        }
+        else
+        {
+            return PowerOf2Iterative(n);
+        }
+    }
+
+
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,61 @@
+using Xunit;
+
+public class AlgorithmsTests
+{
+    // Test for Pow method
+    [Fact]
+    public void Pow_ShouldReturnCorrectResult()
+    {
+        // Arrange
+        var algorithms = new Algorithms();
+        
+        // Act & Assert
+        Assert.Equal(1, algorithms.Pow(2, 0));
+        Assert.Equal(2, algorithms.Pow(2, 1));
+        Assert.Equal(4, algorithms.Pow(2, 2));
+        Assert.Equal(8, algorithms.Pow(2, 3));
+        Assert.Equal(1024, algorithms.Pow(2, 10));
+    }
+
+    // Test for PowerOf2Iterative method
+    [Fact]
+    public void PowerOf2Iterative_ShouldReturnCorrectResult()
+    {
+        // Act & Assert
+        Assert.Equal(1, Algorithms.PowerOf2Iterative(0));
+        Assert.Equal(2, Algorithms.PowerOf2Iterative(1));
+        Assert.Equal(4, Algorithms.PowerOf2Iterative(2));
+        Assert.Equal(8, Algorithms.PowerOf2Iterative(3));
+        Assert.Equal(1024, Algorithms.PowerOf2Iterative(10));
+    }
+
+    // Test for PowerOf2Recursive method
+    [Fact]
+    public void PowerOf2Recursive_ShouldReturnCorrectResult()
+    {
+        // Act & Assert
+        Assert.Equal(1, Algorithms.PowerOf2Recursive(0));
+        Assert.Equal(2, Algorithms.PowerOf2Recursive(1));
+        Assert.Equal(4, Algorithms.PowerOf2Recursive(2));
+        Assert.Equal(8, Algorithms.PowerOf2Recursive(3));
+        Assert.Equal(1024, Algorithms.PowerOf2Recursive(10));
+    }
+
+    // Test for SuperPowerOf2 method
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    [InlineData(2, 4)]
+    [InlineData(3, 8)]
+    [InlineData(10, 1024)]
+    [InlineData(31, 2147483648)] // This is a big number handled by PowerOf2Recursive
+    [InlineData(32, 4294967296)] // This should be handled by PowerOf2Iterative
+    public void SuperPowerOf2_ShouldReturnCorrectResult(int n, long expected)
+    {
+        // Act
+        long result = Algorithms.SuperPowerOf2(n);
+        
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
